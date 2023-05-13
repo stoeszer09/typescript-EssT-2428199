3bcbc734f167b357499af5852b8d8c39931c1b4d
Phil Splettstoeszer <stoeszer09@gmail.com>
2023-05-13 17:21:20 -0500

    Exercise 5 Functions and types

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,16 @@
 interface Contact {
     id: number;
     name: string;
+    clone(name: string): Contact;
 }
 
-function clone(source) {
+function clone(source: Contact): Contact {
     return Object.apply({}, source);
 }
+
+const a: Contact = { 
+    id: 123, 
+    name: 'Homer',
+}
+
+const b = clone(a)
