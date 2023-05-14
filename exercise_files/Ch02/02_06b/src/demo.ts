b3fd0f61f61446bc5f115d57a57d5933e68f3c6b
Phil Splettstoeszer <stoeszer09@gmail.com>
2023-05-13 20:31:05 -0500

    Exercise 6 Adding Generic Types

--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -3,9 +3,20 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
+interface UserContact<TExternalId {
+    id: number,
+    name: string,
+    username: string,
+    externalId: TExternalId,
+    loadExternalId(): Task<TExternalId>,
+}
+
+function clone<myType, mySecondType extends myType>(source: myType): mySecondType {
     return Object.apply({}, source);
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+const b = clone<Contact, UserContact<string>>(a)
+
+const dateRange = { startDate: Date.now(), endDate: Date.now() }
+const dateRangeCopy = clone(dateRange)
